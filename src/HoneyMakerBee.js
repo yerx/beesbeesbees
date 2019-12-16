7a14c2ad77621861544b2e2d579b7bec938a56e0
yerx <yer.x.xiong@gmail.com>
2019-12-16 15:15:37 -0500

    Completed.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  Bee.call(this,'color', 'food');
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+  console.log('this', this);
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+// HoneyMakerBee.prototype = Object.create(Grub.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot -= 1;
+}
 
